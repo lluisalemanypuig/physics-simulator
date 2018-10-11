67b9c7feff7b1a4894e60cf6755397aa43619e37
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-10-11 09:51:46 +0200

    Use rectangles instead of planes

--- examples/particles-renderer/sim3.cpp
+++ examples/particles-renderer/sim3.cpp
@@ -20,10 +20,11 @@ void MainWindow::make_sim3(SimulationRenderer *sr) {
 	const glm::vec3 P(-1.0f, 3.0f, 0.5f);
 	const glm::vec3 S(-1.5f, 5.0f,-1.5f);
 
-	hose h;
+	const glm::vec3 hK = K + vec3(0.0f,0.5f,0.0f);
+	const glm::vec3 hL = L + vec3(0.0f,0.5f,0.0f);
 
-	void set_hose_source(const vec3& _S, const vec3& _u, float _r,float _h);
-	h.set_hose_source(K, glm::normalize(L-K), 0.5f, glm::length(L-K));
+	hose h;
+	h.set_hose_source(hK, glm::normalize(hL-hK), 0.5f, glm::length(hL-hK));
 	h.set_starttime_initialiser(
 		[](particle *p) {
 			p->set_starttime( p->get_index()/1000.0f );
@@ -33,41 +34,41 @@ void MainWindow::make_sim3(SimulationRenderer *sr) {
 	make_init_with_params(h);
 	sr->get_simulator().set_initialiser(&h);
 
-	rplane *floor = new rplane();
+	rrectangle *floor = new rrectangle();
 	floor->p1 = E;
 	floor->p2 = G;
 	floor->p3 = F;
 	floor->p4 = D;
-	floor->pl = new plane(vec3(0.0f,1.0f,0.0f), D);
+	floor->rl = new rectangle(floor->p1,floor->p2,floor->p3, D);
 	sr->add_rgeom(floor);
 
-	rplane *wall1 = new rplane();
+	rrectangle *wall1 = new rrectangle();
 	wall1->p1 = K;
 	wall1->p2 = I;
 	wall1->p3 = E;
 	wall1->p4 = G;
-	wall1->pl = new plane(vec3(0.0f,0.0f,-1.0f), wall1->p1);
+	wall1->rl = new rectangle(wall1->p1,wall1->p2,wall1->p3,wall1->p4);
 	wall1->set_color(QVector4D(1.0f,1.0f,0.0f,1.0f));
-	rplane *wall2 = new rplane();
+	rrectangle *wall2 = new rrectangle();
 	wall2->p1 = K;
 	wall2->p2 = J;
 	wall2->p3 = F;
 	wall2->p4 = G;
-	wall2->pl = new plane(vec3(-1.0f,0.0f,0.0f), wall2->p1);
+	wall2->rl = new rectangle(wall2->p1,wall2->p2,wall2->p3,wall2->p4);
 	wall2->set_color(QVector4D(1.0f,0.0f,1.0f,1.0f));
-	rplane *wall3 = new rplane();
+	rrectangle *wall3 = new rrectangle();
 	wall3->p1 = J;
 	wall3->p2 = H;
 	wall3->p3 = D;
 	wall3->p4 = F;
-	wall3->pl = new plane(vec3(0.0f,0.0f,1.0f), wall3->p1);
+	wall3->rl = new rectangle(wall3->p1,wall3->p2,wall3->p3,wall3->p4);
 	wall3->set_color(QVector4D(0.0f,1.0f,1.0f,1.0f));
-	rplane *wall4 = new rplane();
+	rrectangle *wall4 = new rrectangle();
 	wall4->p1 = H;
 	wall4->p2 = I;
 	wall4->p3 = E;
 	wall4->p4 = D;
-	wall4->pl = new plane(vec3(1.0f,0.0f,0.0f), wall4->p1);
+	wall4->rl = new rectangle(wall4->p1,wall4->p2,wall4->p3,wall4->p4);
 	wall4->set_color(QVector4D(0.0f,0.0f,1.0f,1.0f));
 	sr->add_rgeom(wall1);
 	sr->add_rgeom(wall2);
