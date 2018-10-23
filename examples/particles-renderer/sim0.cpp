6f320ad69044d4b4af5c5030b9642cde963fb7aa
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-10-23 14:02:57 +0200

    Shrinked rectangular source

--- examples/particles-renderer/sim0.cpp
+++ examples/particles-renderer/sim0.cpp
@@ -6,10 +6,10 @@ void MainWindow::make_sim0(SimulationRenderer *sr) {
 	// these parts of the initialiser are scene-dependent
 
 	rect_shower w;
-	w.set_straight_source(vec3(-4.5f,10.0f,-4.5f), 9.5f,9.5f);
+	w.set_straight_source(vec3(-3.0f,10.0f,-3.0f), 6.0f,6.0f);
 	w.set_starttime_initialiser(
 		[](particle *p) {
-			p->set_starttime( 0.0f );
+			p->set_starttime( p->get_index()/1000.0f );
 		}
 	);
 
