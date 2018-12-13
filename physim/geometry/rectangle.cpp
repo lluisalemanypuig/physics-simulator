f8f4b539fba90095e9e959e7f079f0dbce416788
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-12-13 15:40:24 +0100

    Implemented sphere-rectangle intersection test

--- physim/geometry/rectangle.cpp
+++ physim/geometry/rectangle.cpp
@@ -12,6 +12,91 @@ using namespace std;
 
 // LOCAL-DEFINED
 
+enum rectangle_region {
+	r0123,
+	r0, r1, r2, r3,
+	r01, r12, r23, r30
+};
+
+// locate point Y in the triangle (q0,q1,q2)
+static inline
+rectangle_region locate
+(
+	const physim::math::vec2& q0, const physim::math::vec2& q1,
+	const physim::math::vec2& q2, const physim::math::vec2& q3,
+	const physim::math::vec2& q0n0, const physim::math::vec2& q0n3,
+	const physim::math::vec2& q1n0, const physim::math::vec2& q1n1,
+	const physim::math::vec2& q2n1, const physim::math::vec2& q2n2,
+	const physim::math::vec2& q3n2, const physim::math::vec2& q3n3,
+	const physim::math::vec2 Y
+)
+{
+	// inside rectangle
+	if (__pm2_left_aligned(q0,q1, Y) and
+		__pm2_left_aligned(q1,q2, Y) and
+		__pm2_left_aligned(q2,q3, Y) and
+		__pm2_left_aligned(q3,q0, Y)
+	)
+	{
+		return rectangle_region::r0123;
+	}
+
+	/* regions Rij */
+	if (__pm2_left(q0,q0n0, Y) and
+		__pm2_right(q0,q1, Y) and
+		__pm2_right(q1,q1n0, Y)
+	)
+	{
+		return rectangle_region::r01;
+	}
+	if (__pm2_left(q1,q1n1, Y) and
+		__pm2_right(q1,q2, Y) and
+		__pm2_right(q2,q2n1, Y)
+	)
+	{
+		return rectangle_region::r12;
+	}
+	if (__pm2_left(q2,q2n2, Y) and
+		__pm2_right(q2,q3, Y) and
+		__pm2_right(q3,q3n2, Y)
+	)
+	{
+		return rectangle_region::r23;
+	}
+	if (__pm2_left(q3,q3n3, Y) and
+		__pm2_right(q3,q0, Y) and
+		__pm2_right(q0,q0n3, Y)
+	)
+	{
+		return rectangle_region::r30;
+	}
+
+	/* regions Ri */
+
+	// in case of ties with Rij regions
+	// (R2 with R12 or R2 with R20) choose Ri
+	if ((__pm2_left(q1,q1n0, Y) and __pm2_right(q1, q1n1, Y)) or
+		__pm2_aligned(q1,q1n0, Y) or __pm2_aligned(q1,q1n1, Y)
+	)
+	{
+		return rectangle_region::r1;
+	}
+	if ((__pm2_left(q2,q2n1, Y) and __pm2_right(q2,q2n2, Y)) or
+		__pm2_aligned(q2,q2n1, Y) or __pm2_aligned(q2,q2n2, Y)
+	)
+	{
+		return rectangle_region::r2;
+	}
+	if ((__pm2_left(q3,q3n2, Y) and __pm2_right(q3,q3n3, Y)) or
+		__pm2_aligned(q3,q3n2, Y) or __pm2_aligned(q3,q3n3, Y)
+	)
+	{
+		return rectangle_region::r3;
+	}
+
+	return rectangle_region::r0;
+}
+
 namespace physim {
 using namespace math;
 using namespace particles;
@@ -26,20 +111,9 @@ rectangle::rectangle() : geometry() { }
 
 rectangle::rectangle
 (const vec3& _p0,const vec3& _p1, const vec3& _p2,const vec3& _p3)
-: geometry(), pl(plane(_p0,_p1,_p2))
+: geometry()
 {
-	__pm3_assign_v(p0, _p0);
-	__pm3_assign_v(p1, _p1);
-	__pm3_assign_v(p2, _p2);
-	__pm3_assign_v(p3, _p3);
-
-	// make sure that last vertex is on plane...
-	assert(pl.is_inside(p3));
-
-	__pm3_min4(vmin, p0,p1,p2,p3);
-	__pm3_max4(vmax, p0,p1,p2,p3);
-	__pm3_sub_acc_s(vmin, 0.01f);
-	__pm3_add_acc_s(vmax, 0.01f);
+	set_points(_p0, _p1, _p2, _p3);
 }
 
 rectangle::rectangle(const rectangle& r) : geometry(r) {
@@ -47,11 +121,105 @@ rectangle::rectangle(const rectangle& r) : geometry(r) {
 	__pm3_assign_v(p1, r.p1);
 	__pm3_assign_v(p2, r.p2);
 	__pm3_assign_v(p3, r.p3);
+
+	__pm3_assign_v(u0, r.u0);
+	__pm3_assign_v(u1, r.u1);
+	__pm3_assign_v(u2, r.u2);
+
+	__pm2_assign_v(q0, r.q0);
+	__pm2_assign_v(q1, r.q1);
+	__pm2_assign_v(q2, r.q2);
+	__pm2_assign_v(q3, r.q3);
+
+	__pm2_assign_v(e0, r.e0);
+	__pm2_assign_v(e1, r.e1);
+	__pm2_assign_v(e2, r.e2);
+	__pm2_assign_v(e3, r.e3);
+
+	__pm2_assign_v(n0, r.n0);
+	__pm2_assign_v(n1, r.n1);
+	__pm2_assign_v(n2, r.n2);
+	__pm2_assign_v(n3, r.n3);
+
+	__pm2_assign_v(q0n0, r.q0n0);
+	__pm2_assign_v(q0n3, r.q0n3);
+	__pm2_assign_v(q1n0, r.q1n0);
+	__pm2_assign_v(q1n1, r.q1n1);
+	__pm2_assign_v(q2n1, r.q2n1);
+	__pm2_assign_v(q2n2, r.q2n2);
+	__pm2_assign_v(q3n2, r.q3n2);
+	__pm2_assign_v(q3n3, r.q3n3);
 }
 
 rectangle::~rectangle() { }
 
 // SETTERS
+
+void rectangle::set_points
+(const math::vec3& _p0,const math::vec3& _p1,
+ const math::vec3& _p2,const math::vec3& _p3)
+{
+	/* copy vertices */
+	__pm3_assign_v(p0, _p0);
+	__pm3_assign_v(p1, _p1);
+	__pm3_assign_v(p2, _p2);
+	__pm3_assign_v(p3, _p3);
+
+	/* make box */
+	__pm3_min3(vmin, p0,p1,p2);
+	__pm3_max3(vmax, p0,p1,p2);
+	__pm3_sub_acc_s(vmin, 0.01f);
+	__pm3_add_acc_s(vmax, 0.01f);
+
+	/* make vectors u0,u1,u2 */
+	vec3 p0p1, p0p2, p0p3;
+	__pm3_sub_v_v(p0p1, p1, p0);
+	__pm3_sub_v_v(p0p2, p2, p0);
+	__pm3_sub_v_v(p0p3, p3, p0);
+
+	__pm3_cross(u2, p0p1, p0p2);
+	normalise(u2, u2);
+	__pm3_assign_v(u0, p0p1);
+	normalise(u0, u0);
+	__pm3_cross(u1, u2, u0);
+
+	/* make local reference system */
+	float l = __pm3_norm(p0p1);
+	float alpha = __pm3_dot(u0, p0p2);
+	float beta = __pm3_dot(u1, p0p2);
+	float gamma = __pm3_dot(u0, p0p3);
+	float delta = __pm3_dot(u1, p0p3);
+	// points
+	__pm2_assign_s(q0, 0.0f);
+	__pm2_assign_c(q1, l, 0.0f);
+	__pm2_assign_c(q2, alpha, beta);
+	__pm2_assign_c(q2, gamma, delta);
+	// edge vectors
+	__pm2_assign_c(e0, l, 0.0f);
+	__pm2_assign_c(e1, alpha - l, beta);
+	__pm2_assign_c(e2, gamma - alpha, delta - beta);
+	__pm2_assign_c(e3, -gamma, -delta);
+	// normal edge vectors
+	__pm2_assign_c(n0, 0.0f, -1.0f);
+	__pm2_assign_c(n1, beta, l - alpha);
+	normalise(n1, n1);
+	__pm2_assign_c(n2, delta - beta, alpha - gamma);
+	normalise(n2, n2);
+	__pm2_assign_c(n3, -delta, gamma);
+	normalise(n3, n3);
+	// normal edge points
+	__pm2_add_v_v(q0n0, q0, n0);
+	__pm2_add_v_v(q0n3, q0, n3);
+	__pm2_add_v_v(q1n0, q1, n0);
+	__pm2_add_v_v(q1n1, q1, n1);
+	__pm2_add_v_v(q2n1, q2, n1);
+	__pm2_add_v_v(q2n2, q2, n2);
+	__pm2_add_v_v(q3n2, q3, n2);
+	__pm2_add_v_v(q3n3, q3, n3);
+
+	// make plane
+	pl = plane(u2, p1);
+}
 
 void rectangle::set_position(const vec3& v) {
 	__pm3_add_acc_v(p0, v);
@@ -63,6 +231,9 @@ void rectangle::set_position(const vec3& v) {
 	__pm3_add_acc_v(vmax, v);
 	__pm3_sub_acc_s(vmin, 0.01f);
 	__pm3_add_acc_s(vmax, 0.01f);
+
+	// points q0,q1,q2 and vectors u0,u1,u2,n0,n1,n2
+	// do not change since translations preserve distances.
 }
 
 // GETTERS
@@ -72,7 +243,7 @@ const plane& rectangle::get_plane() const {
 }
 
 void rectangle::get_points
-(math::vec3& _p0, math::vec3& _p1, math::vec3& _p2, math::vec3& _p3)
+(vec3& _p0, vec3& _p1, vec3& _p2, vec3& _p3)
 const
 {
 	__pm3_assign_v(_p0, p0);
@@ -81,12 +252,67 @@ const
 	__pm3_assign_v(_p3, p3);
 }
 
-void rectangle::projection(const math::vec3& p, math::vec3& proj) const {
+void rectangle::projection(const vec3& X, vec3& proj) const {
+	vec3 p0X;
+	__pm3_sub_v_v(p0X, X, p0);
 
+	// projection of point onto the plane
+	// (in the local reference system)
+	vec2 Y;
+	__pm2_assign_c(Y, __pm3_dot(u0,p0X), __pm3_dot(u1,p0X));
+
+	// easy regions: r012 (inside rectangle), r0,r1,r2
+	rectangle_region R = locate(q0,q1,q2,q3, q0n0,q0n3,q1n0,q1n1,q2n1,q2n2,q3n2,q3n3, Y);
+	switch (R) {
+	case r0123:
+		__pm3_add_vs_vs_v(proj, u0,__pm3_dot(u0,p0X), u1,__pm3_dot(u1,p0X), p0);
+		break;
+	case r0: __pm3_assign_v(proj, p0); return;
+	case r1: __pm3_assign_v(proj, p1); return;
+	case r2: __pm3_assign_v(proj, p2); return;
+	case r3: __pm3_assign_v(proj, p3); return;
+	default:
+		;
+	}
+
+	// difficult regions (deimited by edges)
+	// r01, r12, r20
+	float s;
+	vec2 qY;
+	switch (R) {
+	case r01:
+		__pm2_sub_v_v(qY, Y, q0);
+		s = __pm2_dot(e0,qY)/__pm2_dot(e0,e0);
+		__pm3_sub_v_v(proj, p1, p0);
+		__pm3_add_v_vs(proj, p0, proj,s);
+		break;
+	case r12:
+		__pm2_sub_v_v(qY, Y, q1);
+		s = __pm2_dot(e1,qY)/__pm2_dot(e1,e1);
+		__pm3_sub_v_v(proj, p2, p1);
+		__pm3_add_v_vs(proj, p1, proj,s);
+		break;
+	case r23:
+		__pm2_sub_v_v(qY, Y, q2);
+		s = __pm2_dot(e2,qY)/__pm2_dot(e2,e2);
+		__pm3_sub_v_v(proj, p3, p2);
+		__pm3_add_v_vs(proj, p2, proj,s);
+		break;
+	case r30:
+		__pm2_sub_v_v(qY, Y, q3);
+		s = __pm2_dot(e3,qY)/__pm2_dot(e3,e3);
+		__pm3_sub_v_v(proj, p0, p3);
+		__pm3_add_v_vs(proj, p3, proj,s);
+		break;
+	default:
+		;
+	}
 }
 
-float rectangle::distance(const math::vec3& p) const {
-	return 0.0f;
+float rectangle::distance(const vec3& p) const {
+	vec3 proj;
+	projection(p, proj);
+	return __pm3_dist(p, proj);
 }
 
 bool rectangle::is_inside(const vec3& p, float tol) const {
@@ -137,9 +363,12 @@ bool rectangle::intersec_segment
 }
 
 bool rectangle::intersec_sphere(const vec3& c, float R) const {
-	cerr << "rectangle::intersec_sphere (" << __LINE__ << ") - Error" << endl;
-	cerr << "    Not implemented yet" << endl;
-	return false;
+	// if the distance between the projection of
+	// the center onto the triangle and the center
+	// is smaller than the radius we have intersection
+	vec3 proj;
+	projection(c, proj);
+	return __pm3_dist2(c, proj) <= R*R;
 }
 
 // OTHERS
@@ -148,23 +377,55 @@ void rectangle::update_particle
 (const vec3& pred_pos, const vec3& pred_vel, free_particle *p)
 const
 {
+	// Since a precondition is that the
+	// segment intersects the triangle...
+	// well, we can just use the plane
 	pl.update_particle(pred_pos, pred_vel, p);
 }
 
 void rectangle::correct_position(
 	const vec3& pred_pos, const sized_particle *p,
-	vec3& correct_position
+	vec3& cor_pos
 ) const
 {
-
+	float D = distance(pred_pos);
+	vec3 vel_normal;
+	normalise(p->cur_vel, vel_normal);
+	// the correct position is the predicted position
+	// moved 'backwards' as much distance as the intersection
+	__pm3_sub_v_vs(cor_pos, pred_pos, vel_normal, p->R - D);
 }
 
 void rectangle::update_particle
 (const vec3& pred_pos, const vec3& pred_vel, sized_particle *p)
 const
 {
-	cerr << "rectangle::update_particle (" << __LINE__ << ") - Error" << endl;
-	cerr << "    Not implemented yet" << endl;
+	// no need to save the position because
+	// the plane will do it for us
+
+	// 0. Compute projection of predicted position
+	// onto the triangle.
+	vec3 proj;
+	projection(pred_pos, proj);
+
+	// 1. Correct the position of the sized particle
+	//    (code copied to avoid recomputation of vectors)
+
+	float D = __pm3_dist(proj, pred_pos);
+
+	vec3 vel_normal;
+	normalise(pred_vel, vel_normal);
+	vec3 cor_pos;
+	__pm3_sub_v_vs(cor_pos, pred_pos, vel_normal, p->R - D);
+
+	// 2. Update the position of the underlying free particle
+	// 2.1. Compute normal of tangent plane
+	vec3 dir;
+	__pm3_sub_v_v(dir, pred_pos, proj);
+	normalise(dir,dir);
+
+	plane T(dir, cor_pos);
+	T.update_particle(pred_pos, pred_vel, static_cast<free_particle *>(p));
 }
 
 void rectangle::display() const {
--- physim/geometry/rectangle.hpp
+++ physim/geometry/rectangle.hpp
@@ -4,6 +4,7 @@
 
 // physim includes
 #include <physim/geometry/geometry.hpp>
+#include <physim/geometry/triangle.hpp>
 #include <physim/geometry/plane.hpp>
 #include <physim/particles/free_particle.hpp>
 #include <physim/particles/sized_particle.hpp>
@@ -36,6 +37,180 @@ class rectangle : public geometry {
 
 		/// Plane associated to the rectangle.
 		plane pl;
+		/**
+		 * @brief Plane parametrisation vector.
+		 *
+		 * \f$ \vec{u_0} = (p_1 - p_0)/||p_1 - p_2|| \f$
+		 */
+		math::vec3 u0;
+		/**
+		 * @brief Plane parametrisation vector.
+		 *
+		 * \f$ \vec{u_1} = \vec{u_2} \times \vec{u_0} \f$
+		 */
+		math::vec3 u1;
+		/**
+		 * @brief Plane parametrisation vector.
+		 *
+		 * \f$ \vec{u_2} =
+		 *		( (p_1-p_0)\times(p_2-p_0) )/
+		 *		|| (p_1-p_0)\times(p_2-p_0) || \f$
+		 */
+		math::vec3 u2;
+
+		/**
+		 * @brief Local reference system point.
+		 *
+		 * \f$q_0 = (0,0)\f$
+		 */
+		math::vec2 q0;
+		/**
+		 * @brief Local reference system point.
+		 *
+		 * \f$q_1 = (l,0), l = ||p_1-p_0||\f$
+		 */
+		math::vec2 q1;
+		/**
+		 * @brief Local reference system point.
+		 *
+		 * \f$q_2 = (\alpha,\beta)\f$,
+		 *
+		 * where
+		 * \f$\alpha = u_0\cdot(p_2-p_0), \beta = u_1\cdot(p_2-p_0)\f$
+		 */
+		math::vec2 q2;
+		/**
+		 * @brief Local reference system point.
+		 *
+		 * \f$q_3 = (\gamma,\delta)\f$,
+		 *
+		 * where
+		 * \f$\gamma = u_0\cdot(p_3-p_0), \delta = u_1\cdot(p_3-p_0)\f$
+		 */
+		math::vec2 q3;
+
+		/**
+		 * @brief Local reference system edge vector.
+		 *
+		 * \f$\vec{e_0} = q_1 - q_0\f$
+		 */
+		math::vec2 e0;
+		/**
+		 * @brief Local reference system edge vector.
+		 *
+		 * \f$\vec{e_1} = q_2 - q_1\f$
+		 */
+		math::vec2 e1;
+		/**
+		 * @brief Local reference system edge vector.
+		 *
+		 * \f$\vec{e_2} = q_3 - q_2\f$
+		 */
+		math::vec2 e2;
+		/**
+		 * @brief Local reference system edge vector.
+		 *
+		 * \f$\vec{e_3} = q_0 - q_3\f$
+		 */
+		math::vec2 e3;
+
+		/**
+		 * @brief Local reference system normal edge vector.
+		 *
+		 * Perpendicular to \f$e_0\f$, see @ref e0.
+		 *
+		 * \f$\vec{n_0} = (0,-1)\f$
+		 */
+		math::vec2 n0;
+		/**
+		 * @brief Local reference system normal edge vector.
+		 *
+		 * Perpendicular to \f$e_1\f$, see @ref e1.
+		 *
+		 * \f$\vec{n_1} =
+		 *		(\beta, l - \alpha)/
+		 *		|| (\beta, l - \alpha) || \f$
+		 *
+		 * where
+		 * \f$\alpha = u_0\cdot(p_2-p_0), \beta = u_1\cdot(p_2-p_0)\f$
+		 */
+		math::vec2 n1;
+		/**
+		 * @brief Local reference system normal edge vector.
+		 *
+		 * Perpendicular to \f$e_2\f$, see @ref e2.
+		 *
+		 * \f$\vec{n_2} =
+		 *		(\delta - \beta, \alpha - \gamma)/
+		 *		|| (\delta - \beta, \alpha - \gamma) || \f$
+		 *
+		 * where
+		 * \f$\alpha = u_0\cdot(p_2-p_0), \beta = u_1\cdot(p_2-p_0)\f$,
+		 * \f$\gamma = u_0\cdot(p_3-p_0), \delta = u_1\cdot(p_3-p_0)\f$
+		 */
+		math::vec2 n2;
+		/**
+		 * @brief Local reference system normal edge vector.
+		 *
+		 * Perpendicular to \f$e_3\f$, see @ref e3.
+		 *
+		 * \f$\vec{n_3} =
+		 *		(-\delta, \gamma)/
+		 *		|| (-\delta, \gamma) ||\f$
+		 *
+		 * where
+		 * \f$\gamma = u_0\cdot(p_3-p_0), \delta = u_1\cdot(p_3-p_0)\f$
+		 */
+		math::vec2 n3;
+
+		/**
+		 * @brief Local reference system normal edge point.
+		 *
+		 * \f$q_0 + \vec{n_0}\f$
+		 */
+		math::vec2 q0n0;
+		/**
+		 * @brief Local reference system normal edge point.
+		 *
+		 * \f$q_0 + \vec{n_3}\f$
+		 */
+		math::vec2 q0n3;
+		/**
+		 * @brief Local reference system normal edge point.
+		 *
+		 * \f$q_1 + \vec{n_0}\f$
+		 */
+		math::vec2 q1n0;
+		/**
+		 * @brief Local reference system normal edge point.
+		 *
+		 * \f$q_1 + \vec{n_1}\f$
+		 */
+		math::vec2 q1n1;
+		/**
+		 * @brief Local reference system normal edge point.
+		 *
+		 * \f$q_2 + \vec{n_1}\f$
+		 */
+		math::vec2 q2n1;
+		/**
+		 * @brief Local reference system normal edge point.
+		 *
+		 * \f$q_2 + \vec{n_2}\f$
+		 */
+		math::vec2 q2n2;
+		/**
+		 * @brief Local reference system normal edge point.
+		 *
+		 * \f$q_3 + \vec{n_2}\f$
+		 */
+		math::vec2 q3n2;
+		/**
+		 * @brief Local reference system normal edge point.
+		 *
+		 * \f$q_3 + \vec{n_3}\f$
+		 */
+		math::vec2 q3n3;
 
 	public:
 		/// Default constructor.
@@ -69,6 +244,21 @@ class rectangle : public geometry {
 		// SETTERS
 
 		/**
+		 * @brief Set the points of this rectangle.
+		 *
+		 * The plane associated to this rectangle (see @ref pl)
+		 * is built using these vertices in the same order they
+		 * are given in this method.
+		 *
+		 * See
+		 * @ref plane::plane(const math::vec3&,const math::vec3&,const math::vec3&)
+		 * to see how the normal is determined.
+		 */
+		void set_points
+		(const math::vec3& _p0,const math::vec3& _p1,
+		 const math::vec3& _p2,const math::vec3& _p3);
+
+		/**
 		 * @brief Sets the position of this rectangle.
 		 *
 		 * The vertices of the rectangle are translated according
@@ -95,13 +285,13 @@ class rectangle : public geometry {
 		const;
 
 		/**
-		 * @brief Project a point to this triangle.
+		 * @brief Project a point onto this rectangle.
 		 *
-		 * The projected point is the closest point inside the triangle
+		 * The projected point is the closest point inside the rectangle
 		 * to point @e p.
-		 * @param[in] p Point to measure the distance to the triangle.
+		 * @param[in] p Point to be projected onto the rectangle.
 		 * @param[out] proj Projection of @e p onto the closest point to it
-		 * inside the triangle.
+		 * inside the rectangle.
 		 */
 		void projection(const math::vec3& p, math::vec3& proj) const;
 
