a0087de9d1c20714529bfd7f1a8a2b93f058c09f
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-12-18 17:52:17 +0100

    'More correct' simulation of agent particles

--- physim/sim_agent_particles.cpp
+++ physim/sim_agent_particles.cpp
@@ -15,8 +15,6 @@ using namespace math;
 
 void simulator::_simulate_agent_particles() {
 
-	vec3 dir;
-
 	for (size_t i = 0; i < aps.size(); ++i) {
 		agent_particle *p = aps[i];
 
@@ -39,32 +37,37 @@ void simulator::_simulate_agent_particles() {
 		if (p->starttime > 0.0f) {
 			continue;
 		}
+		*/
 
 		// Particles age: reduce their lifetime.
 		p->reduce_lifetime(dt);
-		*/
+
+		/* ------------------ */
+		/*   compute forces   */
 
 		// clear the current force
 		__pm3_assign_s(p->force, 0.0f);
-		// compute forces for particle p,
-		// using fields and others
-		compute_forces(p);
-		// compute force from its attractor
-		__pm3_sub_v_v(dir, p->attractor, p->cur_pos);
-		normalise(dir,dir);
-		float extra = std::sin(__pm3_angle(dir, p->cur_vel)) + 1.0f;
-		__pm3_add_acc_vs(p->force, dir, p->attractor_acceleration*extra);
 
-		// apply solver to predict next position and
-		// velocity of the particle
-		vec3 pred_pos, pred_vel;
-		apply_solver(p, pred_pos, pred_vel);
+		// particle agents are not affected by force fields -.-'
 
-		// scale velocity down to max_vel
-		float speed2 = __pm3_norm2(pred_vel);
-		if (speed2 >= p->desired_vel*p->desired_vel) {
-			__pm3_assign_vs(pred_vel, pred_vel, p->desired_vel/std::sqrt(speed2));
-		}
+		// compute steering force
+		__pm3_sub_v_v(p->force, p->attractor, p->cur_pos);
+		truncate(p->force, p->max_force, p->force);
+
+		/* -------------------------- */
+		/*   apply Euler integration  */
+		/*  to calculate new velocity */
+		/*         and position       */
+
+		vec3 pred_vel, pred_pos;
+		// compute new velocity
+		__pm3_add_v_vs(pred_vel, p->cur_vel, p->force, dt/p->mass);
+		truncate(pred_vel, p->max_speed, pred_vel);
+		// compute new position
+		__pm3_add_v_vs(pred_pos, p->cur_pos, pred_vel, dt);
+
+		/* --------------------------- */
+		/*   apply Euler integration   */
 
 		// collision prediction:
 		// copy the particle at its current state and use it
@@ -94,6 +97,22 @@ void simulator::_simulate_agent_particles() {
 		if (part_part_colls_activated()) {
 			find_update_partcoll_agent(p, i);
 		}
+
+		// here we should compute a bit more steering:
+
+		/* seek steering */
+		vec3 desired_vel;
+		__pm3_sub_v_v(desired_vel, p->attractor, p->cur_pos);
+		normalise(desired_vel, desired_vel);
+		__pm3_mul_acc_s(desired_vel, p->max_speed);
+		vec3 steer_dir;
+		__pm3_sub_v_v(steer_dir, desired_vel, p->cur_vel);
+
+		/* modify agent's velocity */
+		__pm3_add_acc_v(p->cur_vel, steer_dir);
+
+		// finally, truncate velocity again
+		truncate(p->cur_vel, p->max_speed, p->cur_vel);
 	}
 
 }
