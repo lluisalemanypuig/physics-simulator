6e531f0030e472a85f4b571b68551c752ed5f31b
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-12-28 23:59:03 +0100

    Better basic simulation

--- examples/command-line/sim_300.cpp
+++ examples/command-line/sim_300.cpp
@@ -19,10 +19,6 @@ using namespace math;
 using namespace geometric;
 using namespace fluids;
 
-#define side__ 64
-#define slice__ (side__*side__)
-#define total__ (side__*side__*side__)
-
 namespace study_cases {
 
 	void sim_300_usage() {
@@ -30,33 +26,38 @@ namespace study_cases {
 		cout << endl;
 		cout << "Options:" << endl;
 		cout << endl;
-		cout << "    --total-time t: total time of the simulation.         Default: 2.0" << endl;
-		cout << "    --step t:       time step of the simulation.          Default: 0.01" << endl;
-		cout << "    --solver s:     numerical solver to use.              Default: 'semi-euler'" << endl;
+		cout << "    --total-time t: total time of the simulation.  Default: 2.0" << endl;
+		cout << "    --step t:       time step of the simulation.   Default: 0.0001" << endl;
+		cout << "    --solver s:     numerical solver to use.       Default: 'semi-euler'" << endl;
 		cout << "        euler:      Euler integration method.." << endl;
 		cout << "        semi-euler: Euler semi-implicit integration method." << endl;
 		cout << "        verlet:     Verlet integration method." << endl;
-		cout << "    --R r:			 Size of the neighbourhood.            Default: 0.01" << endl;
-		cout << "    --vol v:		 Volume of the fluid.                  Default: 1.0" << endl;
-		cout << "    --vis v:		 Viscosity of the fluid.               Default: 1.0" << endl;
-		cout << "    --D d:			 Density at rest of the fluid.         Default: 1.0" << endl;
-		cout << "    --threads n:    Number of threads.                    Default: 1" << endl;
+		cout << "    --R r:			 Size of the neighbourhood.     Default: 0.06" << endl;
+		cout << "    --vol v:		 Volume of the fluid.           Default: 10.0" << endl;
+		cout << "    --vis v:		 Viscosity of the fluid.        Default: 0.001" << endl;
+		cout << "    --dens d:       Density at rest of the fluid.  Default: 1000.0" << endl;
+		cout << "    --c v:			 Speed of sound.                Default: 1500.0" << endl;
+		cout << "    --p n:			 Number of particles per side.  Default: 16" << endl;
+		cout << "    --threads n:    Number of threads.             Default: 1" << endl;
+		cout << endl;
 	}
 
 	void sim_300(int argc, char *argv[]) {
-		float dt = 0.01f;
+		float dt = 0.0001f;
 		float total_time = 5.0f;
 		solver_type solv = solver_type::EulerSemi;
-		// number of particles
-		size_t N = total__;
+		// number of particles per side
+		size_t side__ = 16;
 		// neighbourhood size
-		float R = 0.01f;
+		float R = 0.06f;
 		// volume
-		float vol = 1.0f;
+		float vol = 10.0f;
 		// viscosity
-		float vis = 1.0f;
+		float vis = 0.001f;
 		// density
-		float D = 1.0f;
+		float dens = 1000.0f;
+		// speed of sound
+		float cs = 1500.0f;
 		// number of threads
 		size_t nt = 1;
 
@@ -85,12 +86,24 @@ namespace study_cases {
 				vis = atof(argv[i + 1]);
 				++i;
 			}
-			else if (strcmp(argv[i], "--D") == 0) {
-				D = atof(argv[i + 1]);
+			else if (strcmp(argv[i], "--dens") == 0) {
+				dens = atof(argv[i + 1]);
+				++i;
+			}
+			else if (strcmp(argv[i], "--c") == 0) {
+				cs = atof(argv[i + 1]);
+				++i;
+			}
+			else if (strcmp(argv[i], "--p") == 0) {
+				side__ = atoi(argv[i + 1]);
 				++i;
 			}
 			else if (strcmp(argv[i], "--threads") == 0) {
 				nt = atoi(argv[i + 1]);
+				++i;
+			}
+			else if (strcmp(argv[i], "--particles") == 0) {
+				side__ = atoi(argv[i + 1]);
 				++i;
 			}
 			else if (strcmp(argv[i], "--solver") == 0) {
@@ -116,13 +129,17 @@ namespace study_cases {
 			}
 		}
 
+		size_t slice__ = side__*side__;
+		size_t total__ = side__*side__*side__;
+		size_t N = total__;
+
 		timing::time_point begin, end;
 
 		cout << "Fluid characteristics:" << endl;
 		cout << "    Number of particles: " << N << endl;
 		cout << "    Volume: " << vol << endl;
 		cout << "    Viscosity: " << vis << endl;
-		cout << "    Density: " << D << endl;
+		cout << "    Density: " << dens << endl;
 		cout << "    Neighbourhood size: " << R << endl;
 		cout << "Simulation:" << endl;
 		cout << "    total time: " << total_time << endl;
@@ -141,9 +158,35 @@ namespace study_cases {
 		cout << "Allocating..." << endl;
 		begin = timing::now();
 		fluid F;
-		F.allocate(N, vol, D, vis, R);
+		F.allocate(N, vol, dens, vis, R, cs);
 		end = timing::now();
 		cout << "    in " << timing::elapsed_seconds(begin, end) << " seconds" << endl;
+
+		cout << "Mass per particle: " << F.get_particles()[0].mass << endl;
+
+		kernel_function W =
+		[R](const fluid_particle&, const fluid_particle&, float r2) -> float
+		{
+			float k = R*R - r2;
+			float PI = static_cast<float>(M_PI);
+			return (315.0f/(64.0f*PI*std::pow(R, 9.0f)))*k*k*k;
+		};
+		kernel_function gW =
+		[R](const fluid_particle&, const fluid_particle&, float r2) -> float
+		{
+			float r = std::sqrt(r2);
+			float k = R*R - r2;
+			float PI = static_cast<float>(M_PI);
+			return (-945.0f/(32.0f*PI*std::pow(R, 9.0f)))*k*k*r;
+		};
+		kernel_function g2W =
+		[R](const fluid_particle&, const fluid_particle&, float r2) -> float
+		{
+			float k = R*R - r2;
+			float PI = static_cast<float>(M_PI);
+			return (945.0f/(8.0f*PI*std::pow(R, 9.0f)))*k*(r2 - 0.75f*k);
+		};
+		F.set_kernel(W, gW, g2W);
 
 		// assign initial positions
 		cout << "Assigning initial positions..." << endl;
@@ -155,7 +198,7 @@ namespace study_cases {
 			size_t col = __i%side__;
 
 			F.get_particles()[i].cur_pos = vec3(
-				row*0.05f, col*0.05f, slice*0.05f
+				row*(0.5f/side__), col*(0.5f/side__), slice*(0.5f/side__)
 			);
 		}
 		end = timing::now();
@@ -167,12 +210,21 @@ namespace study_cases {
 		end = timing::now();
 		cout << "    in " << timing::elapsed_seconds(begin, end) << " seconds" << endl;
 
-		// output forces
 		const fluid_particle *ps = F.get_particles();
+
+		// output forces
+		cout << "FORCES" << endl;
 		for (size_t i = 0; i < N; ++i) {
 			cout << i << ": "
 				 << ps[i].force.x << " " << ps[i].force.y << " " << ps[i].force.z
 				 << endl;
+		}
+
+		// compute velocities
+		cout << "VELOCITIES" << endl;
+		for (size_t i = 0; i < N; ++i) {
+			vec3 vel = ps[i].force*dt*(1.0f/ps[i].mass);
+			cout << i << ": " << vel.x << " " << vel.y << " " << vel.z << endl;
 		}
 	}
 
