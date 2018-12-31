924b027a96c32ec75dca8e75df42ed5a97933411
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-12-31 17:04:39 +0100

    Added option to record interface

--- examples/interfaces/basic-viewer/main.cpp
+++ examples/interfaces/basic-viewer/main.cpp
@@ -21,6 +21,8 @@ using namespace std;
 // custom includes
 #include <basic-viewer/utils.hpp>
 
+#include <png.h>
+
 typedef pair<int,int> point;
 
 // ------------------
@@ -36,6 +38,10 @@ static bool use_shader = false;
 
 static timing::time_point sec;
 
+static int window_width;
+static int window_height;
+
+static unsigned int counter;
 static int FPS;
 static int fps_count;
 static bool display_fps_count;
@@ -45,6 +51,8 @@ static point last_mouse;
 
 static bool lock_mouse;
 static int window_id;
+
+static bool record;
 
 // glut helpers
 #define ESC 27
@@ -60,17 +68,119 @@ static int window_id;
 template<typename T>
 static inline void UNUSED(const T& x) { (void)x; }
 
-static inline
-bool inside_window(int x, int y) {
-	return ((0 <= x) and (x <= SR.window_width()))
-	   and ((0 <= y) and (y <= SR.window_height()));
+/* Write a png file */
+void write_png(const string& name, unsigned char *data, uint w, uint h) {
+	FILE *fp;
+	png_structp png_ptr;
+	png_infop info_ptr;
+
+	/* Open the file */
+	fp = fopen(name.c_str(), "wb");
+	if (fp == nullptr) {
+		return;
+	}
+
+	/* Create and initialize the png_struct with the desired error handler
+	* functions.  If you want to use the default stderr and longjump method,
+	* you can supply NULL for the last three parameters.  We also check that
+	* the library version is compatible with the one used at compile time,
+	* in case we are using dynamically linked libraries.  REQUIRED.
+	*/
+	png_ptr = png_create_write_struct
+	(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+
+	if (png_ptr == nullptr) {
+		fclose(fp);
+		return;
+	}
+
+	/* Allocate/initialize the image information data.  REQUIRED */
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == nullptr) {
+		fclose(fp);
+		png_destroy_write_struct(&png_ptr,  png_infopp_NULL);
+		return;
+	}
+
+	/* Set error handling.  REQUIRED if you aren't supplying your own
+	* error handling functions in the png_create_write_struct() call.
+	*/
+	if (setjmp(png_jmpbuf(png_ptr))) {
+		/* If we get here, we had a problem writing the file */
+		fclose(fp);
+		png_destroy_write_struct(&png_ptr, &info_ptr);
+		return;
+	}
+
+	/* Set up the output control if you are using standard C streams */
+	png_init_io(png_ptr, fp);
+
+	/* Set the image information here.  Width and height are up to 2^31,
+	 * bit_depth is one of 1, 2, 4, 8, or 16, but valid values also depend on
+	 * the color_type selected. color_type is one of PNG_COLOR_TYPE_GRAY,
+	 * PNG_COLOR_TYPE_GRAY_ALPHA, PNG_COLOR_TYPE_PALETTE, PNG_COLOR_TYPE_RGB,
+	 * or PNG_COLOR_TYPE_RGB_ALPHA.  interlace is either PNG_INTERLACE_NONE or
+	 * PNG_INTERLACE_ADAM7, and the compression_type and filter_type MUST
+	 * currently be PNG_COMPRESSION_TYPE_BASE and PNG_FILTER_TYPE_BASE. REQUIRED
+	 */
+	png_set_IHDR
+	(png_ptr, info_ptr, w, h, 8,
+		PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_ADAM7,
+		PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+	png_uint_32 width, height;
+	int bit_depth, color_type, interlace_type, compression_type, filter_method;
+
+	png_get_IHDR(png_ptr, info_ptr, &width, &height,
+		   &bit_depth, &color_type, &interlace_type,
+		   &compression_type, &filter_method);
+
+	/* Optional significant bit (sBIT) chunk */
+	png_color_8 sig_bit;
+	/* If we are dealing with a grayscale image then */
+	sig_bit.gray = 0;
+	/* Otherwise, if we are dealing with a color image then */
+	sig_bit.red = 1;
+	sig_bit.green = 1;
+	sig_bit.blue = 1;
+	/* If the image has an alpha channel then */
+	sig_bit.alpha = 1;
+	png_set_sBIT(png_ptr, info_ptr, &sig_bit);
+
+	png_write_info(png_ptr, info_ptr);
+
+	png_byte **row_pointers =
+		static_cast<png_byte **>(malloc(h*sizeof(png_byte *)));
+
+	for (uint i = 0; i < w*h; i += w) {
+		row_pointers[i/w] = &data[4*i];
+	}
+	png_write_image(png_ptr, row_pointers);
+
+	/* Similarly, if you png_malloced any data that you passed in with
+	* png_set_something(), such as a hist or trans array, free it here,
+	* when you can be sure that libpng is through with it.
+	*/
+	png_free(png_ptr, nullptr);
+
+	/* Whenever you use png_free() it is a good idea to set the pointer to
+	* NULL in case your application inadvertently tries to png_free() it
+	* again.  When png_free() sees a NULL it returns without action, thus
+	* avoiding the double-free security problem.
+	*/
+
+	/* Clean up after the write, and free any memory allocated */
+	png_destroy_write_struct(&png_ptr, &info_ptr);
+
+	/* Close the file */
+	fclose(fp);
 }
 
 // -----------
 // Exit viewer
 // -----------
 
-void __psv_exit_func() {
+void exit_func() {
 	m->clear();
 	delete m;
 }
@@ -79,7 +189,7 @@ void __psv_exit_func() {
 // INITIALISE OpenGL
 // -----------------
 
-void __psv_initGL(int argc, char *argv[]) {
+void initGL(int argc, char *argv[]) {
 	if (argc > 1) {
 		if (strcmp(argv[1], "--use-shaders") == 0) {
 			use_shader = true;
@@ -96,15 +206,15 @@ void __psv_initGL(int argc, char *argv[]) {
 	}
 
 	// initial window size
-	int iw = 640;
-	int ih = 480;
+	window_width = 640;
+	window_height = 480;
 
 	// ----------------- //
 	/* initialise window */
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(50, 25);
-	glutInitWindowSize(iw, ih);
+	glutInitWindowSize(window_width, window_height);
 	window_id = glutCreateWindow("Basic viewer");
 
 	GLenum err = glewInit();
@@ -154,11 +264,6 @@ void __psv_initGL(int argc, char *argv[]) {
 	last_mouse = point(0,0);
 	lock_mouse = false;
 
-	sec = timing::now();
-	display_fps_count = true;
-	FPS = 60;
-	fps_count = 0;
-
 	// ------------------------ //
 	/* load models for geometry */
 	m = new rendered_triangle_mesh();
@@ -171,7 +276,7 @@ void __psv_initGL(int argc, char *argv[]) {
 	m->make_box(model_box);
 	SR.get_box().set_min_max(model_box.get_min(), model_box.get_max());
 
-	SR.set_window_dims(iw, ih);
+	SR.set_window_dims(window_height, window_height);
 	SR.init_cameras();
 
 	// ---------------------------------- //
@@ -192,13 +297,20 @@ void __psv_initGL(int argc, char *argv[]) {
 		m->load_textures();
 		m->compile();
 	}
+
+	counter = 0;
+
+	display_fps_count = true;
+	FPS = 60;
+	fps_count = 0;
+	sec = timing::now();
 }
 
 // ------------
 // RENDER SCENE
 // ------------
 
-void __psv_refresh() {
+void refresh() {
 	++fps_count;
 
 	glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -250,13 +362,31 @@ void __psv_refresh() {
 		SR.get_box().slow_render();
 	}
 
+
+	if (record) {
+		unsigned char *screen =
+		static_cast<unsigned char *>(
+			malloc(window_width*window_height*4*sizeof(unsigned char))
+		);
+
+		glReadPixels
+		(0,0, window_width, window_height, GL_RGBA, GL_UNSIGNED_BYTE, screen);
+
+		string name = std::to_string(counter) + ".png";
+		write_png(name, screen, window_width, window_height);
+
+		free(screen);
+
+		++counter;
+	}
+
 	glutSwapBuffers();
 }
 
-void __psv_timed_refresh(int value) {
+void timed_refresh(int value) {
 	UNUSED(value);
 
-	__psv_refresh();
+	refresh();
 
 	timing::time_point here = timing::now();
 	double elapsed = timing::elapsed_seconds(sec, here);
@@ -268,14 +398,14 @@ void __psv_timed_refresh(int value) {
 		sec = timing::now();
 	}
 
-	glutTimerFunc(1000.0f/FPS, __psv_timed_refresh, 0);
+	glutTimerFunc(1000.0f/FPS, timed_refresh, 0);
 }
 
 // ---------------
 // RESIZE viewport
 // ---------------
 
-void __psv_reshape(int w, int h) {
+void reshape(int w, int h) {
 	float pzoom = SR.get_perspective_camera().get_zoom();
 	float ozoom = SR.get_orthogonal_camera().get_zoom();
 
@@ -286,6 +416,9 @@ void __psv_reshape(int w, int h) {
 	SR.get_orthogonal_camera().set_zoom(ozoom);
 
 	glViewport(0, 0, w, h);
+
+	window_width = w;
+	window_height = h;
 }
 
 // ++++++++++++++++++
@@ -296,7 +429,7 @@ void __psv_reshape(int w, int h) {
 // MOUSE HANDLER
 // -------------
 
-void __psv_mouse_click_event(int button, int state, int x, int y) {
+void mouse_click_event(int button, int state, int x, int y) {
 	UNUSED(x);
 	UNUSED(y);
 
@@ -321,7 +454,7 @@ void __psv_mouse_click_event(int button, int state, int x, int y) {
 	pressed_button = button;
 }
 
-void __psv_mouse_movement(int x, int y) {
+void mouse_movement(int x, int y) {
 	int dx = x - last_mouse.first;
 	int dy = y - last_mouse.second;
 	last_mouse = point(x,y);
@@ -335,7 +468,7 @@ void __psv_mouse_movement(int x, int y) {
 	}
 }
 
-void __psv_mouse_drag_event(int x, int y) {
+void mouse_drag_event(int x, int y) {
 	int dx = x - last_mouse.first;
 	int dy = y - last_mouse.second;
 	last_mouse = point(x,y);
@@ -355,13 +488,13 @@ void __psv_mouse_drag_event(int x, int y) {
 // KEYBOARD HANDLER
 // ----------------
 
-void __psv_special_keys(int key, int x, int y) {
+void special_keys(int key, int x, int y) {
 	UNUSED(key);
 	UNUSED(x);
 	UNUSED(y);
 }
 
-void __psv_keyboard_event(unsigned char c, int x, int y) {
+void keyboard_event(unsigned char c, int x, int y) {
 	UNUSED(x);
 	UNUSED(y);
 
@@ -398,6 +531,19 @@ void __psv_keyboard_event(unsigned char c, int x, int y) {
 	else if (c == 'x') {
 		display_fps_count = not display_fps_count;
 	}
+	else if (c == 'r') {
+		record = not record;
+		if (record) {
+			cout << endl << endl << endl;
+			cout << "RECORDING!" << endl;
+			cout << endl << endl << endl;
+		}
+		else {
+			cout << endl << endl << endl;
+			cout << "Stop recording." << endl;
+			cout << endl << endl << endl;
+		}
+	}
 	else {
 		if (SR.is_flying()) {
 			if (c == 'w') {
@@ -417,20 +563,20 @@ void __psv_keyboard_event(unsigned char c, int x, int y) {
 }
 
 int main(int argc, char* argv[]) {
-	__psv_initGL(argc, argv);
+	initGL(argc, argv);
 
-	atexit(__psv_exit_func);
+	atexit(exit_func);
 
-	glutDisplayFunc(__psv_refresh);
-	glutReshapeFunc(__psv_reshape);
-	glutKeyboardFunc(__psv_keyboard_event);
-	glutSpecialFunc(__psv_special_keys);
-	glutMouseFunc(__psv_mouse_click_event);
-	glutPassiveMotionFunc(__psv_mouse_movement);
-	glutMotionFunc(__psv_mouse_drag_event);
+	glutDisplayFunc(refresh);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard_event);
+	glutSpecialFunc(special_keys);
+	glutMouseFunc(mouse_click_event);
+	glutPassiveMotionFunc(mouse_movement);
+	glutMotionFunc(mouse_drag_event);
 
 	//glutIdleFunc(refresh);
-	glutTimerFunc(1000.0f/FPS, __psv_timed_refresh, 0);
+	glutTimerFunc(1000.0f/FPS, timed_refresh, 0);
 
 	glutMainLoop();
 }
