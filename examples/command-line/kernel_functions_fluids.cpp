3e799823b4c6079615083b2b7010da07dbeb9a02
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2019-01-03 19:57:09 +0100

    Fixed bugs in kernel functions

--- examples/command-line/kernel_functions_fluids.cpp
+++ examples/command-line/kernel_functions_fluids.cpp
@@ -4,23 +4,23 @@ using namespace physim;
 using namespace math;
 using namespace fluids;
 
+static
+const float PI = static_cast<float>(M_PI);
+
 namespace kernel_functions {
 
 void density_poly6(float H, kernel_scalar_function& f) {
 	f = [H](float r2) -> float
 	{
-		float k = 1.0f - r2/(H*H);
-		float PI = static_cast<float>(M_PI);
-		return (315.0f/(64.0f*PI*std::pow(H, 3.0f)))*k*k*k;
+		float k = 1.0f/H - r2*(1.0f/(H*H*H));
+		return (315.0f/(64.0f*PI))*k*k*k;
 	};
 }
 void pressure_poly6(float H, kernel_vectorial_function& f) {
 	f = [H](const vec3& r, float r2, vec3& res) -> void
 	{
-		static const float PI = static_cast<float>(M_PI);
-
 		float k = 1.0f - r2/(H*H);
-		float s = (-945.0f/(32.0f*PI*std::pow(H, 3.0f)))*k*k;
+		float s = (-945.0f/(32.0f*PI*std::pow(H, 5.0f)))*k*k;
 		res = r*s;
 	};
 }
@@ -28,8 +28,7 @@ void viscosity_poly6(float H, kernel_scalar_function& f) {
 	f = [H](float r2) -> float
 	{
 		float k = 1.0f - r2/(H*H);
-		float PI = static_cast<float>(M_PI);
-		return (945.0f/(8.0f*PI*std::pow(H, 7.0f)))*k*(r2/(H*H) - 0.75f*k);
+		return (945.0f/(8.0f*PI*std::pow(H, 5.0f)))*k*(r2/(H*H) - 0.75f*k);
 	};
 }
 
--- examples/interfaces/fluids/kernels.hpp
+++ examples/interfaces/fluids/kernels.hpp
@@ -5,8 +5,8 @@
 
 namespace kernel_functions {
 
-	void density_poly6(float H, physim::fluids::kernel_scalar_function& f);
-	void pressure_poly6(float H, physim::fluids::kernel_vectorial_function& f);
-	void viscosity_poly6(float H, physim::fluids::kernel_scalar_function& f);
+void density_poly6(float H, physim::fluids::kernel_scalar_function& f);
+void pressure_poly6(float H, physim::fluids::kernel_vectorial_function& f);
+void viscosity_poly6(float H, physim::fluids::kernel_scalar_function& f);
 
 } // -- namespace kernel_functions
