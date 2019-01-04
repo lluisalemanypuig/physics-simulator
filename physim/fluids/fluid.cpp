cd98c86a52c50c6a934d3ea65984367da8fabec6
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2019-01-04 11:27:40 +0100

    Algorithm seems to be correct

--- physim/fluids/fluid.cpp
+++ physim/fluids/fluid.cpp
@@ -13,8 +13,6 @@ using namespace std;
 #include <physim/math/private/math3.hpp>
 #include <physim/math/vec3.hpp>
 
-#define sq(x) (x*x)
-
 namespace physim {
 using namespace math;
 using namespace particles;
@@ -23,6 +21,9 @@ using namespace structures;
 namespace fluids {
 
 // PROTECTED
+
+#define sq(x) ((x)*(x))
+#define inv(x) (1.0f/(x))
 
 #define make_neighbours_lists(i, neighs, d2s)					\
 	tree->get_indices(ps[i].cur_pos, R, neighs);				\
@@ -62,25 +63,31 @@ namespace fluids {
 	ps[i].density += ps[i].mass*kernel(0.0f);					\
 	ps[i].pressure = sq(speed_sound)*(ps[i].density - density);
 
-#define update_force(i, neighs, d2s)							\
-	vec3 dir, aux;												\
-	vec3 acc; /* total acceleration */							\
-	for (size_t j_it = 0; j_it < neighs.size(); ++j_it) {		\
-		size_t j = neighs[j_it];								\
-		__pm3_sub_v_v(dir, ps[j].cur_pos, ps[i].cur_pos);		\
-		float Pij = -ps[j].mass*(								\
-			ps[i].pressure*(1.0f/sq(ps[i].density)) +			\
-			ps[j].pressure*(1.0f/sq(ps[j].density))				\
-		);														\
-		kernel_pressure(dir, d2s[j_it], aux);					\
-		__pm3_add_acc_vs(acc, aux, Pij);						\
-		float visc_coef = ps[i].density*ps[j].density;			\
-		visc_coef *= viscosity*ps[j].mass*(1.0f/visc_coef);		\
-		visc_coef *= kernel_viscosity(d2s[j_it]);				\
-		__pm3_sub_v_v_mul_s										\
-		(aux, ps[i].cur_vel, ps[j].cur_vel,visc_coef);			\
-		__pm3_add_acc_v(acc, aux);								\
-	}															\
+#define update_force(i, neighs, d2s)								\
+	/* total acceleration */										\
+	vec3 acc;														\
+	/* auxiliary vectors */											\
+	vec3 part_i_to_j, pressure_dir, press_acc, visc_acc;			\
+	for (size_t j_it = 0; j_it < neighs.size(); ++j_it) {			\
+		size_t j = neighs[j_it];									\
+		/* pressure acceleration */									\
+		__pm3_sub_v_v(part_i_to_j, ps[j].cur_pos, ps[i].cur_pos);	\
+		float Pij = -ps[j].mass*(									\
+			ps[i].pressure*inv(sq(ps[i].density)) +					\
+			ps[j].pressure*inv(sq(ps[j].density))					\
+		);															\
+		kernel_pressure(part_i_to_j, d2s[j_it], pressure_dir);		\
+		__pm3_assign_vs(press_acc, pressure_dir,Pij);				\
+		__pm3_add_acc_v(acc, press_acc);							\
+		/* viscosity acceleration */								\
+		float Vij = ps[i].density*ps[j].density;					\
+		Vij *= viscosity*ps[j].mass*inv(Vij);						\
+		Vij *= kernel_viscosity(d2s[j_it]);							\
+		__pm3_sub_v_v_mul_s											\
+		(visc_acc, ps[i].cur_vel,ps[j].cur_vel, Vij);				\
+		/* accumulate acceleration */								\
+		__pm3_add_acc_v(acc, visc_acc);								\
+	}																\
 	__pm3_assign_vs(ps[i].force, acc, ps[i].mass);
 
 // PUBLIC
@@ -150,7 +157,7 @@ void fluid::allocate
 	}
 
 	if (ps == nullptr) {
-		cerr << "fluid::allocate (" << __LINE__ << ") - Error:" << endl;
+		cerr << "fluid::allocate (:" << __LINE__ << ") - Error:" << endl;
 		cerr << "    Could not allocate memory for "
 			 << N << " particles (" << N*sizeof(fluid_particle) << " bytes)"
 			 << endl;
@@ -180,25 +187,23 @@ void fluid::update_forces() {
 	// between a particle and its neighbours.
 	// We need a dynamic list for the neighbours, but a
 	// fixed-size list of these lists will suffice.
-	vector<vector<size_t> > neighs(N);
-	vector<vector<float> > d2s(N);
-
-	cout << "----------------" << endl;
+	vector<vector<size_t> > all_neighs(N);
+	vector<vector<float> > all_d2s(N);
 
 	// Obtain neighbours list. Then filter them to have only
 	// the closest neighbours.
 	for (size_t i = 0; i < N; ++i) {
-		make_neighbours_lists(i, neighs[i], d2s[i]);
+		make_neighbours_lists(i, all_neighs[i], all_d2s[i]);
 	}
 
 	// compute density and pressure of each particle
 	for (size_t i = 0; i < N; ++i) {
-		initialise_densities_pressures(i, neighs[i], d2s[i]);
+		initialise_densities_pressures(i, all_neighs[i], all_d2s[i]);
 	}
 
 	// compute forces of the fluid (due to pressure and viscosity)
 	for (size_t i = 0; i < N; ++i) {
-		update_force(i, neighs[i], d2s[i]);
+		update_force(i, all_neighs[i], all_d2s[i]);
 	}
 }
 
@@ -212,26 +217,26 @@ void fluid::update_forces(size_t n) {
 	// between a particle and its neighbours.
 	// We need a dynamic list for the neighbours, but a
 	// fixed-size list of these lists will suffice.
-	vector<vector<size_t> > neighs(N);
-	vector<vector<float> > d2s(N);
+	vector<vector<size_t> > all_neighs(N);
+	vector<vector<float> > all_d2s(N);
 
 	// Obtain neighbours list. Then filter them to have only
 	// the closest neighbours.
 	#pragma omp parallel for num_threads(n)
 	for (size_t i = 0; i < N; ++i) {
-		make_neighbours_lists(i, neighs[i], d2s[i]);
+		make_neighbours_lists(i, all_neighs[i], all_d2s[i]);
 	}
 
 	// compute density and pressure of each particle
 	#pragma omp parallel for num_threads(n)
 	for (size_t i = 0; i < N; ++i) {
-		initialise_densities_pressures(i, neighs[i], d2s[i]);
+		initialise_densities_pressures(i, all_neighs[i], all_d2s[i]);
 	}
 
 	// compute forces of the fluid (due to pressure and viscosity)
 	#pragma omp parallel for num_threads(n)
 	for (size_t i = 0; i < N; ++i) {
-		update_force(i, neighs[i], d2s[i]);
+		update_force(i, all_neighs[i], all_d2s[i]);
 	}
 }
 
