103f1ec96eb28677ede06274cc14398afb739bf5
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2019-01-09 17:06:08 +0100

    couts for debugging

--- physim/fluids/newtonian.cpp
+++ physim/fluids/newtonian.cpp
@@ -103,6 +103,9 @@ newtonian::~newtonian() {
 void newtonian::update_forces() {
 	make_partition();
 
+	/*cout << "R= " << R << endl;
+	cout << "R^2= " << sq(R) << endl;*/
+
 	// Compute neighbour lists, and squared distances
 	// between a particle and its neighbours.
 	// We need a dynamic list for the neighbours, but a
@@ -110,20 +113,130 @@ void newtonian::update_forces() {
 	vector<vector<size_t> > all_neighs(N);
 	vector<vector<float> > all_d2s(N);
 
+//#if defined (FAULTY)
+	//cout << "Neighbours:" << endl;
 	// Obtain neighbours list. Then filter them to have only
 	// the closest neighbours.
 	for (size_t i = 0; i < N; ++i) {
-		make_neighbours_lists(i, all_neighs[i], all_d2s[i]);
+		tree->get_indices(ps[i].cur_pos, R, all_neighs[i]);
+		all_d2s[i] = vector<float>(all_neighs[i].size());
+		/* resort by distances: put those farther than R
+		 * at the back of the vector */
+		size_t lim = all_neighs[i].size();
+
+		size_t j_it = 0;
+		while (j_it < lim) {
+			size_t j = all_neighs[i][j_it];
+			float d2 = __pm3_dist2(ps[i].cur_pos, ps[j].cur_pos);
+			all_d2s[i][j_it] = d2;
+
+			if (d2 > sq(R) or j == i) {
+				/* Put this element at the end. (to be removed)
+				 * Do not advance 'j_it' */
+				swap(all_neighs[i][j_it], all_neighs[i][lim - 1]);
+				swap(all_d2s[i][j_it], all_d2s[i][lim - 1]);
+				--lim;
+			}
+			else {
+				/* this element is OK. Move forward. */
+				++j_it;
+			}
+		}
+		if (lim < all_neighs[i].size()) {
+			/* remove last elements */
+			all_neighs[i].erase( all_neighs[i].begin() + lim, all_neighs[i].end() );
+			all_d2s[i].erase( all_d2s[i].begin() + lim, all_d2s[i].end() );
+		}
 	}
+//#endif
+
+#if defined (SAFE)
+	cout << "Neighbours:" << endl;
+	for (size_t i = 0; i < N; ++i) {
+		cout << "    " << i << ":" << endl;
+		for (size_t j = 0; j < N; ++j) {
+			if (i == j) {
+				continue;
+			}
+			float d2 = __pm3_dist2(ps[i].cur_pos, ps[j].cur_pos);
+			if (d2 <= sq(R)) {
+				all_neighs[i].push_back(j);
+				all_d2s[i].push_back(d2);
+			}
+		}
+
+		cout << "        neighbourhood size: " << all_neighs[i].size() << endl;
+		cout << "           ";
+		for (size_t j = 0; j < all_neighs[i].size(); ++j) {
+			cout << " " << all_neighs[i][j];
+		}
+		cout << endl;
+	}
+#endif
+
+	//cout << "Density and pressure" << endl;
 
 	// compute density and pressure of each particle
 	for (size_t i = 0; i < N; ++i) {
-		initialise_densities_pressures(i, all_neighs[i], all_d2s[i]);
+		//cout << "    " << i << ":" << endl;
+
+		ps[i].density = 0.0f;
+		/* iterate over the list of neighbours.
+		 * initialise density, pressure, and store squared
+		 * distance */
+		for (size_t j_it = 0; j_it < all_neighs[i].size(); ++j_it) {
+			size_t j = all_neighs[i][j_it];
+			float d2 = all_d2s[i][j_it];
+
+			ps[i].density += ps[j].mass*kernel_density(d2);
+			/*cout << "        ++ " << ps[i].density
+				 << ". ! " << j << ": " << ps[j].mass*kernel_density(d2)
+				 << endl;
+			cout << "            ps[" << j << "].mass= " << ps[j].mass << endl;
+			cout << "            ps[" << j << "].cur_pos= "
+				 << __pm3_out(ps[j].cur_pos) << endl;
+			cout << "            all_d2s[" << i << "][" << j_it << "]= "
+				 << d2 << endl;
+			cout << "            W_density()= "
+				 << kernel_density(d2) << endl;*/
+
+		}
+		ps[i].density += ps[i].mass*kernel_density(0.0f);
+		/*cout << "        D: ++ " << ps[i].density
+			 << ". ! " << i << ": " << ps[i].mass*kernel_density(0.0f)
+			 << endl;*/
+
+		ps[i].pressure = sq(speed_sound)*(ps[i].density - density);
+		//cout << "        P: " << ps[i].pressure << endl;
 	}
 
 	// compute forces of the fluid (due to pressure and viscosity)
 	for (size_t i = 0; i < N; ++i) {
-		update_force(i, all_neighs[i], all_d2s[i]);
+		/* total acceleration */
+		vec3 acc;
+		/* auxiliary vectors */
+		vec3 part_i_to_j, pressure_dir, press_acc, visc_acc;
+		for (size_t j_it = 0; j_it < all_neighs[i].size(); ++j_it) {
+			size_t j = all_neighs[i][j_it];
+			/* pressure acceleration */
+			__pm3_sub_v_v(part_i_to_j, ps[j].cur_pos, ps[i].cur_pos);
+			float Pij = -ps[j].mass*(
+				ps[i].pressure*inv(sq(ps[i].density)) +
+				ps[j].pressure*inv(sq(ps[j].density))
+			);
+			kernel_pressure(part_i_to_j, all_d2s[i][j_it], pressure_dir);
+			__pm3_assign_vs(press_acc, pressure_dir,Pij);
+			__pm3_add_acc_v(acc, press_acc);
+			/* viscosity acceleration */
+			float Vij = ps[i].density*ps[j].density;
+			Vij *= viscosity*ps[j].mass*inv(Vij);
+			Vij *= kernel_viscosity(all_d2s[i][j_it]);
+			__pm3_sub_v_v_mul_s
+			(visc_acc, ps[i].cur_vel,ps[j].cur_vel, Vij);
+			/* accumulate acceleration */
+			__pm3_add_acc_v(acc, visc_acc);
+		}
+		__pm3_assign_vs(ps[i].force, acc, ps[i].mass);
 	}
 }
 
