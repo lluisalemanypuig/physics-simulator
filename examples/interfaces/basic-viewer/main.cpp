f8e5e470c24680332b2f790444111ee214d60327
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2019-10-03 10:03:28 +0200

    Updated LICENSE

--- examples/interfaces/basic-viewer/main.cpp
+++ examples/interfaces/basic-viewer/main.cpp
@@ -1,3 +1,24 @@
+/*********************************************************************
+ * Real-time physics simulation project
+ * Copyright (C) 2018-2019 Lluís Alemany Puig
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contact: Lluís Alemany Puig (lluis.alemany.puig@gmail.com)
+ * 
+ ********************************************************************/
+
 // C includes
 #include <string.h>
 
@@ -563,6 +584,11 @@ void keyboard_event(unsigned char c, int x, int y) {
 }
 
 int main(int argc, char* argv[]) {
+	cout << "Real-time physics simulation project  Copyright (C) 2018-2019  Lluís Alemany Puig" << endl;
+	cout << "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'." << endl;
+	cout << "This is free software, and you are welcome to redistribute it" << endl;
+	cout << "under certain conditions; type `show c' for details." << endl;
+	
 	initGL(argc, argv);
 
 	atexit(exit_func);
